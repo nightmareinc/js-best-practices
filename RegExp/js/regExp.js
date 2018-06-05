f6d3cac6ca7f55cdbcd3ee7efc48f1b60eea82c8
Hossein Azadi <hossein.azadi14@outlook.com>
2018-06-05 16:30:38 +0430

    changed the search event to click

--- RegExp/js/regExp.js
+++ RegExp/js/regExp.js
@@ -1,30 +1,27 @@
 'use strict';
 var timeoutFunc;
-document.getElementById('searchText').addEventListener('keyup', function () {
-    if (timeoutFunc)
-        clearTimeout(timeoutFunc);
-    timeoutFunc = setTimeout(function () {
-        var incomingInput = document.getElementById('incomingInput').value,
-            pattern = document.getElementById('searchText').value,
-            regExp = new RegExp(pattern, 'gm');
+document.getElementById('doSearch').addEventListener('click', function () {
+    var incomingInput = document.getElementById('incomingInput').value,
+        pattern = document.getElementById('searchText').value,
+        regExp = new RegExp(pattern, 'gm');
 
-        var matchingArray,
-            resultString = '<p>',
-            first = 0,
-            last = 0;
-        if (pattern !== '') {
-            while ((matchingArray = regExp.exec(incomingInput)) != null) {
-                last = matchingArray.index;
-                resultString += incomingInput.substring(first, last)
+    var matchingArray,
+        resultString = '<p>',
+        first = 0,
+        last = 0;
+    if (pattern !== '') {
+        while ((matchingArray = regExp.exec(incomingInput)) != null) {
+            last = matchingArray.index;
+            resultString += incomingInput.substring(first, last)
 
-                resultString += '<span class="found">' + matchingArray[0] + '</span>'
-                first = regExp.lastIndex
-            }
-            resultString += incomingInput.substring(first, incomingInput.length)
-            resultString += '</p>'
-            document.getElementById('searchResults').innerHTML = resultString;
+            resultString += '<span class="found">' + matchingArray[0] + '</span>'
+            first = regExp.lastIndex
         }
-    }, 500)
+        resultString += incomingInput.substring(first, incomingInput.length)
+        resultString += '</p>'
+        document.getElementById('searchResults').innerHTML = resultString;
+
+    }
 
 });
 document.getElementById('doReplace').addEventListener('click', function () {
